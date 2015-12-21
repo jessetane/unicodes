594b3138d008f49dce3909ebcf66f45d4b4fc423
Jesse Tane <jesse.tane@gmail.com>
2015-12-21 03:07:26 -0500

    fix safari detection

--- src/ua/index.js
+++ src/ua/index.js
@@ -2,7 +2,8 @@ var ua = window.navigator.userAgent
 
 exports.small = window.screen.height < 768
 exports.android = /Android/.test(ua)
-exports.safari = /AppleWebKit/.test(ua)
+exports.chrome = /Chrome/.test(ua)
+exports.safari = !exports.chrome && /AppleWebKit/.test(ua)
 exports.ios = exports.safari && /iPod|iPhone|iPad/.test(ua)
 exports.touch = exports.android || exports.ios // how to add FF OS and MS things?
 
